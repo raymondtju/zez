ceaf35dbfdca1e564dbfe433435bf276aa39543c
raymondtju <raymond.rtju@gmail.com>
2023-02-14 17:01:46 +0700

    switch to dev env

--- src/configs/index.js
+++ src/configs/index.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_BE_API,
+  baseURL: process.env.NEXT_PUBLIC_DEV_BE_API,
 });
 
 export default instance;
