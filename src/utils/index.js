c0c6f69343194578b40a64bba4c7e386f20684de
raymondtju <raymond.rtju@gmail.com>
2023-02-14 17:01:19 +0700

    add authorization header

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,7 +1,12 @@
 import axios from "../configs";
 
-export async function postData(url, payload) {
-  return await axios.post(`${url}`, payload);
+export async function postData(url, payload, token) {
+  return await axios.post(`${url}`, {
+    ...payload,
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  });
 }
 
 export async function fetch(url, payload) {
