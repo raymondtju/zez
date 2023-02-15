650217b8825fc84c7a73b3edf95feff2d5fc187d
raymondtju <raymond.rtju@gmail.com>
2023-02-15 16:06:11 +0700

    if !token

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,12 +1,22 @@
 import axios from "../configs";
+import Cookies from "js-cookie";
 
-export async function postData(url, payload, token) {
-  return await axios.post(`${url}`, {
-    ...payload,
-    headers: {
-      Authorization: `Bearer ${token}`,
-    },
-  });
+export async function postData(url, payload) {
+  let token = Cookies.get("token");
+
+  if (!token) {
+    token = process.env.NEXT_PUBLIC_GUEST_TOKEN;
+  }
+
+  try {
+    return await axios.post(`${url}`, payload, {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    });
+  } catch (error) {
+    return error;
+  }
 }
 
 export async function fetch(url, payload) {
