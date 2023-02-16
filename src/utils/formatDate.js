df69b5085a55f30f25c9578b1abf52496d177536
raymondtju <raymond.rtju@gmail.com>
2023-02-16 14:37:44 +0700

    format date function

---
+++ src/utils/formatDate.js
@@ -0,0 +1,31 @@
+export function formatDate(date) {
+  const d = new Date(date);
+  // console.log(d);
+  const dtf = new Intl.DateTimeFormat("en", {
+    year: "numeric",
+    month: "short",
+    day: "2-digit",
+    hour: "numeric",
+    minute: "numeric",
+    hour: "numeric",
+    timeZone: "Asia/Jakarta",
+    timeZoneName: "short",
+  });
+  const [
+    { value: mo },
+    ,
+    { value: da },
+    ,
+    { value: ye },
+    ,
+    { value: hour },
+    ,
+    { value: minute },
+    ,
+    { value: timeZone },
+    ,
+    { value: timeZoneName },
+  ] = dtf.formatToParts(d);
+
+  return `${da} ${mo} ${ye} ${hour}:${minute} ${timeZone}, ${timeZoneName}`;
+}
