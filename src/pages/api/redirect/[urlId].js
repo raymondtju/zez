5559406e30b36b95e8855322bd967ac52452b0c8
raymondtju <raymond.rtju@gmail.com>
2023-02-16 15:43:52 +0700

    fix frontend api redirect

--- src/pages/api/redirect/[urlId].js
+++ src/pages/api/redirect/[urlId].js
@@ -1,12 +1,15 @@
-import { fetch } from "@/utils";
+import { fetchData } from "@/utils";
 
 const redirect = async (req, res) => {
   const { urlId } = req.query;
   try {
-    const check = await fetch(`${process.env.NEXT_PUBLIC_BE_API}/${urlId}`);
-    // console.log(check);
+    const check = await fetchData(`${process.env.NEXT_PUBLIC_BE_API}/${urlId}`);
+    if (check.data.message === "Url not found") {
+      res.redirect(307, "/");
+    }
     res.redirect(307, `${process.env.NEXT_PUBLIC_BE_API}/${urlId}`);
   } catch (error) {
+    console.log(error);
     res.redirect(307, "/");
   }
   //   const check = await fetch(`${process.env.BE_API}/${urlId}`);
