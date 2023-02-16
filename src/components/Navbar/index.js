f5c2fe2cd139a2a87ba8086500c0b7d94978c25c
raymondtju <raymond.rtju@gmail.com>
2023-02-16 14:35:35 +0700

    add logout function

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,16 +1,26 @@
 import React, { useEffect, useState } from "react";
 import Image from "next/image";
+import { useRouter } from "next/router";
 import Cookies from "js-cookie";
 
 import GithubIcon from "/public/icons/github.svg";
 import Link from "next/link";
+import { ArrowLeftOnRectangleIcon } from "@heroicons/react/24/solid";
 
 export default function Navbar() {
-  const [token, setToken] = useState("");
+  const router = useRouter();
+  const [isToken, setIsToken] = useState(false);
+
+  const root = router.pathname === "/";
 
   useEffect(() => {
-    setToken(Cookies.get("token"));
-  }, [token]);
+    Cookies.get("token") ? setIsToken(true) : setIsToken(false);
+  }, [root, isToken]);
+
+  const handleLogout = () => {
+    Cookies.remove("token");
+    router.reload();
+  };
 
   return (
     <nav className="flex items-center justify-between py-4">
@@ -29,26 +39,39 @@ export default function Navbar() {
             <Image src={GithubIcon} alt="Github" />
           </button>
         </div>
-        {token ? (
-          <div className="flex items-center gap-4">
-            <Link href="/dashboard">
-              <button
-                className={`hover: rounded-lg py-2 px-4 shadow-lg hover:shadow-inner`}
-              >
-                <span>Dashboard</span>
-              </button>
-            </Link>
-          </div>
-        ) : (
-          <div className="flex items-center gap-4">
-            <Link href="/signin">
-              <button
-                className={`hover: rounded-lg py-2 px-4 shadow-lg hover:shadow-inner`}
-              >
-                <span>Sign In</span>
-              </button>
-            </Link>
-          </div>
+        {router.pathname !== "/signin" && (
+          <>
+            {isToken ? (
+              <div className="flex items-center gap-2">
+                <Link href="/dashboard">
+                  <button
+                    className={`hover: rounded-lg py-2 px-4 shadow-lg hover:shadow-inner`}
+                  >
+                    <span>Dashboard</span>
+                  </button>
+                </Link>
+
+                <button
+                  className={`hover: block h-full rounded-lg p-2 shadow-lg hover:shadow-inner`}
+                  onClick={handleLogout}
+                >
+                  <span>
+                    <ArrowLeftOnRectangleIcon className="w-5 h-5 fill-red-600" />
+                  </span>
+                </button>
+              </div>
+            ) : (
+              <div className="flex items-center gap-4">
+                <Link href="/signin">
+                  <button
+                    className={`hover: rounded-lg py-2 px-4 shadow-lg hover:shadow-inner`}
+                  >
+                    <span>Sign In</span>
+                  </button>
+                </Link>
+              </div>
+            )}
+          </>
         )}
       </div>
     </nav>
