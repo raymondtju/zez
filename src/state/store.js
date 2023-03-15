1164dd49e5e30b12cfe20df7b18cd89e048313f2
raymondtju <raymond.rtju@gmail.com>
2023-03-15 20:20:19 +0700

    update: remove persist

--- src/state/store.js
+++ src/state/store.js
@@ -1,26 +1,47 @@
 import { combineReducers, configureStore } from "@reduxjs/toolkit";
 import { themeSlice } from "./theme/slice";
-import storage from "redux-persist/lib/storage";
-import { persistReducer, persistStore } from "redux-persist";
-import thunk from "redux-thunk";
 import { userSlice } from "./user/slice";
+// import createWebStorage from "redux-persist/lib/storage/createWebStorage";
 
-const persistConfig = {
-  key: "root",
-  storage,
-};
+// import thunk
+
+// const createNoopStorage = () => {
+//   return {
+//     getItem(_key) {
+//       return Promise.resolve(null);
+//     },
+//     setItem(_key, value) {
+//       return Promise.resolve(value);
+//     },
+//     removeItem(_key) {
+//       return Promise.resolve();
+//     },
+//   };
+// };
+
+// const storage =
+//   typeof window === "undefined" ? createNoopStorage() : createWebStorage();
+
+// import { persistReducer, persistStore } from "redux-persist";
+// import thunk from "redux-thunk";
+// import { userSlice } from "./user/slice";
+
+// const persistConfig = {
+//   key: "persist-store",
+//   storage,
+// };
 
 const reducers = combineReducers({
   theme: themeSlice.reducer,
   user: userSlice.reducer,
 });
 
-const persistedReducer = persistReducer(persistConfig, reducers);
+// const persistedReducer = persistReducer(persistConfig, reducers);
 
 export const store = configureStore({
-  reducer: persistedReducer,
-  // devTools: process.env.NODE_ENV !== "production",
-  middleware: [thunk],
+  reducer: reducers,
+  devTools: process.env.NODE_ENV !== "production",
+  // middleware: [thunk],
 });
 
-export const persistor = persistStore(store);
+// export const persistor = persistStore(store);
