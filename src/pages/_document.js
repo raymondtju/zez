c066c117939ee2d05be5e85e113c5fe8785b7831
raymondtju <raymond.rtju@gmail.com>
2023-03-15 20:26:16 +0700

    add: universal styling at _document

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,13 +1,20 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import clsx from "clsx";
+import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
     <Html lang="en">
       <Head />
-      <body>
+      <body
+        className={clsx(
+          `bg-zinc-100 font-satoshi selection:bg-gray-500 selection:text-white`,
+          "dark:bg-zinc-900 ",
+          "duration-200 ease-in-out"
+        )}
+      >
         <Main />
         <NextScript />
       </body>
     </Html>
-  )
+  );
 }
