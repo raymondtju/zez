ad69df33981766699cce6babdf1770f3815b96a3
raymondtju <raymond.rtju@gmail.com>
2023-04-21 00:12:14 +0700

    fix styling and responsive

--- components/Navbar/index.tsx
+++ components/Navbar/index.tsx
@@ -17,6 +17,7 @@ import {
 } from "@heroicons/react/24/solid";
 import { useAppDispatch, useAppSelector } from "@/hooks";
 import { signOut } from "next-auth/react";
+import Button from "../Button";
 
 export default function Navbar({ session }: { session?: any }) {
   let theme = useAppSelector((state) => state.theme.value);
@@ -50,7 +51,7 @@ export default function Navbar({ session }: { session?: any }) {
   return (
     <nav
       className={clsx(
-        "sticky top-0 z-10 w-full border-b-[0.1rem] border-primary bg-zinc-100/30 text-primary backdrop-blur-xl",
+        "sticky top-0 z-10 w-full border-b-[1px] border-slate-600 bg-zinc-100/30 text-primary backdrop-blur-xl",
         "dark:border-zinc-100 dark:bg-primary/30 dark:text-zinc-100"
       )}
     >
@@ -58,40 +59,44 @@ export default function Navbar({ session }: { session?: any }) {
         <div className="flex items-center justify-between py-2">
           <div className="flex items-center gap-1">
             <Link href="/">
-              <h1 className="text-2xl font-extrabold ">kraa.cc</h1>
+              <h1 className="text-lg font-extrabold md:text-xl lg:text-2xl ">
+                kraa.cc
+              </h1>
             </Link>
             <small className="flex items-end">(alpha)</small>
           </div>
 
           <div
             className={clsx(
-              `flex w-auto flex-row items-center gap-x-6 border-none sm:static`
+              `flex w-auto flex-row items-center sm:gap-x-6 gap-x-2 border-none sm:static`
             )}
           >
-            <Link href={"https://github.com/raymondtju"} target="_blank">
+            {/* <Link href={"https://github.com/raymondtju"} target="_blank">
               <div className="flex items-center gap-2 font-semibold underline underline-offset-4 hover:decoration-2">
-                <GiftIcon className="h-5 w-5" />
+                <GiftIcon className="w-5 h-5" />
                 <span>Github</span>
               </div>
-            </Link>
+            </Link> */}
 
+            {/* <button
+              className={clsx(
+                `border-spacing-0 rounded-full border-2 border-primary bg-zinc-100 px-5 py-2 font-bold text-primary`,
+                "duration-300 hover:border-2 hover:bg-primary hover:text-zinc-100",
+                "dark:border-2 dark:border-zinc-100 dark:bg-primary dark:text-zinc-100 dark:hover:bg-zinc-100 dark:hover:text-primary"
+              )}
+            >
+              Get Started
+            </button> */}
+            
             {router.pathname === "/" && !isToken && (
               <div className="flex items-center gap-4">
                 <Link href="/auth/signin">
-                  <button
-                    className={clsx(
-                      `border-spacing-0 rounded-full border-2 border-primary bg-zinc-100 px-5 py-2 font-bold text-primary`,
-                      "duration-300 hover:border-2 hover:bg-primary hover:text-zinc-100",
-                      "dark:border-2 dark:border-zinc-100 dark:bg-primary dark:text-zinc-100 dark:hover:bg-zinc-100 dark:hover:text-primary"
-                    )}
-                  >
-                    Get Started
-                  </button>
+                  <Button className="px-3 py-1.5 sm:px-4 sm:py-2">Signin</Button>
                 </Link>
               </div>
             )}
 
-            {router.pathname === "/auth/signup" && (
+            {/* {router.pathname === "/auth/signup" || router.pathname === "/auth/signin" && (
               <div className="flex items-center gap-4">
                 <Link href="/auth/signin">
                   <button
@@ -101,47 +106,31 @@ export default function Navbar({ session }: { session?: any }) {
                       "dark:border-2 dark:border-zinc-100 dark:bg-primary dark:text-zinc-100 dark:hover:bg-zinc-100 dark:hover:text-primary"
                     )}
                   >
-                    Sign In
+                    Signin
                   </button>
                 </Link>
-              </div>
-            )}
-
-            {router.pathname === "/auth/signin" && (
-              <div className="flex items-center gap-4">
-                <Link href="/auth/signup">
-                  <button
-                    className={clsx(
-                      `border-spacing-0 rounded-full border-2 border-primary bg-zinc-100 px-5 py-2 font-bold text-primary`,
-                      "duration-300 hover:border-2 hover:bg-primary hover:text-zinc-100",
-                      "dark:border-2 dark:border-zinc-100 dark:bg-primary dark:text-zinc-100 dark:hover:bg-zinc-100 dark:hover:text-primary"
-                    )}
-                  >
-                    Sign Up
-                  </button>
-                </Link>
-              </div>
-            )}
+              </div> 
+            )} */}
 
             <button className="flex items-center" onClick={handleMenu}>
-              <Bars2Icon className="h-6 w-6" />
+              <Bars2Icon className="w-6 h-6" />
             </button>
             <div
               className={clsx(
-                "flex transition-all duration-300 ease-in-out",
+                "transition-all duration-300 ease-in-out",
                 `${
-                  isMenu ? "fixed inset-0 h-[100vh] backdrop-blur-xl" : "hidden"
+                  isMenu ? "fixed inset-0 h-[100vh] backdrop-blur-sm bg-white/90" : "hidden"
                 }`
               )}
             >
-              <div className="fixed inset-0 h-[100vh] bg-zinc-100/70 backdrop-blur-xl transition-all dark:bg-zinc-900/70" />
+              {/* <div className="fixed inset-0 h-[100vh] bg-zinc-100/90 backdrop-blur-md transition-all dark:bg-zinc-900/90 w-screen" /> */}
               <Layout>
-                <div className="relative float-right flex flex-col justify-end gap-3 duration-300">
+                <div className="relative flex flex-col justify-end float-right gap-3 duration-300">
                   <button
-                    className="absolute right-0 top-4 flex justify-end"
+                    className="absolute right-0 flex justify-end top-4"
                     onClick={handleMenu}
                   >
-                    <XMarkIcon className="h-6 w-6" />
+                    <XMarkIcon className="w-6 h-6" />
                   </button>
 
                   <button
@@ -153,9 +142,9 @@ export default function Navbar({ session }: { session?: any }) {
                     )}
                   >
                     {theme ? (
-                      <SunIcon className="h-6 w-6" />
+                      <SunIcon className="w-6 h-6" />
                     ) : (
-                      <MoonIcon className="h-6 w-6" />
+                      <MoonIcon className="w-6 h-6" />
                     )}
 
                     <span>{`Theme mode: ${theme ? "dark" : "light"}`}</span>
@@ -172,7 +161,7 @@ export default function Navbar({ session }: { session?: any }) {
                           )}
                           onClick={handleMenu}
                         >
-                          <HomeIcon className="h-6 w-6" />
+                          <HomeIcon className="w-6 h-6" />
                           <span>Dashboard</span>
                         </button>
                       </Link>
@@ -185,7 +174,7 @@ export default function Navbar({ session }: { session?: any }) {
                           )}
                           onClick={handleMenu}
                         >
-                          <LinkIcon className="h-6 w-6" />
+                          <LinkIcon className="w-6 h-6" />
                           <span>Links</span>
                         </button>
                       </Link>
@@ -199,7 +188,7 @@ export default function Navbar({ session }: { session?: any }) {
                         onClick={() => signOut({ callbackUrl: "/" })}
                       >
                         <span>
-                          <ArrowLeftOnRectangleIcon className="h-6 w-6" />
+                          <ArrowLeftOnRectangleIcon className="w-6 h-6" />
                           <span>Logout</span>
                         </span>
                       </button>
